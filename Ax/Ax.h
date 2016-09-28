ade5ca03c553e27f5ccadacd85045c6971d91faf
Wilson Balderrama <wilson.balderrama@gmail.com>
2016-09-28 18:32:52 -0400

    init

---
+++ Ax/Ax.h
@@ -0,0 +1,19 @@
+//
+//  Ax.h
+//  Ax
+//
+//  Created by Wilson Balderrama on 9/28/16.
+//  Copyright © 2016 Wilson Balderrama. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Ax.
+FOUNDATION_EXPORT double AxVersionNumber;
+
+//! Project version string for Ax.
+FOUNDATION_EXPORT const unsigned char AxVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Ax/PublicHeader.h>
+
+
